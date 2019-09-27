e8e50f2b2b24e142f5d3e21dd7a965bee64a5527
Kenneth Mahlatse Mokhethi <kennethmahlatsemokhethi@gmail.com>
2019-09-27 13:17:11 +0200

    Tested to ensure only integers are excepted on the setProperties function,the length of the properties is equal to sides otherwise throw an error

--- spec/diceSpec.js
+++ spec/diceSpec.js
@@ -14,12 +14,36 @@ describe("Checking the sides range", () => {
 });
 
 describe("Error handling", () => {
+  it("Ensure the length of propabilities array is equal to the sides of the dice", () => {
+    let dieDodgy6 = new Dice(6);
+    try {
+      dieDodgy6.setProbabilities([1, 1, 1, 1, 2]);
+    } catch (e) {
+      expect(e).toBeTruthy("The length of the array is not equal to the sides");
+    }
+  });
   it("Check negative propability", () => {
     let dieDodgy6 = new Dice(6);
     try {
       dieDodgy6.setProbabilities([1, 1, 1, 1, -1, 2]);
     } catch (e) {
       expect(e).toBeTruthy("negative probabilities not allowed");
+    }
+  });
+
+  it("Ensure the sum of propabilities is greater than otherwise", () => {
+    try {
+      dieDodgy6.setProbabilities([0, 0, 0, 0, 0, 0]);
+    } catch (e) {
+      expect(e).toBeTruthy("probability sum must be greater than 0");
+    }
+  });
+
+  it("Ensure that setPropertities only excerpt intger value", () => {
+    try {
+      dieDodgy6.setProbabilities([1, 1, 1, 1.5, 1, 1]);
+    } catch (e) {
+      expect(e).toBeTruthy("only integer values allowed");
     }
   });
 });
